a467385a803b6825c2517dd97891fbf40aea681f
Ramchandra2612 <ram.salgaonkar2612@gmail.com>
2025-06-10 11:02:22 -0700

    Add files via upload

---
+++ T1.py
@@ -0,0 +1,21 @@
+# a=int(input('Enter first number'))
+# b=int(input('Enter Second number'))
+
+# C=(b+a)
+# d=(b-a)
+# E=(b*a)
+# F=(b/a)
+
+
+
+# print("Addition:", C)
+# print("Subtraction:", d)
+# print("Multiplication : ",E)
+# print("Division : ",F)
+
+
+
+user_1=input("Enter your first name :")
+user_2=input("Enter your last name :")
+
+print("Hello "+user_1 +" " +user_2+"!"+" Welcome to the python program")
